037d236a698bc4e892a0ad5c12835cd27deb96a6
Steve Wolf <morris.brujah@gmail.com>
2022-05-18 21:32:20 +0200

    hexadecimal finished

--- hexadecimal.js
+++ hexadecimal.js
@@ -4,14 +4,13 @@ console.log(ascii_to_hexa("12")); //3132
 console.log(ascii_to_hexa("100")); //313030
  */
 
-function ascii_to_hexa (string)   {
-	let array = [];
-	for (let i = 0; i < string.length; i++) 
-     {
-		let hexaDeci = Number(string.charCodeAt(i)).toString(16);
-		array.push(hexaDeci);
-	 }
-	return array.join('');
+function ascii_to_hexa(string) {
+  let array = [];
+  for (let i = 0; i < string.length; i++) {
+    let hexaDeci = Number(string.charCodeAt(i)).toString(16);
+    array.push(hexaDeci);
+  }
+  return array.join("");
 }
 console.log(ascii_to_hexa("12")); //3132
 console.log(ascii_to_hexa("100")); //313030
