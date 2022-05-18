a1af0f2c443f63e4bd508b39279fe62e7406dd53
Steve Wolf <morris.brujah@gmail.com>
2022-05-18 21:24:39 +0200

    alphabatized finished now for real

--- alphabatize.js
+++ alphabatize.js
@@ -1,12 +1,16 @@
 /**
-* Write a JavaScript function to alphabetize a given string.
-*Note : Alphabetize string : An individual string can be alphabetized. This rearranges the letters so they are sorted A to Z.
-*console.log(alphabetize_string("United States"));
-*"SUadeeinsttt"
-*/
+ * Write a JavaScript function to alphabetize a given string.
+ *Note : Alphabetize string : An individual string can be alphabetized. This rearranges the letters so they are sorted A to Z.
+ *console.log(alphabetize_string("United States"));
+ *"SUadeeinsttt"
+ */
 
-function alphabetize (string) {
-  let array = string.split("")
-  return array.sort((a,b) => b < a).join("")
+function alphabetize(string) {
+  return string.split('').sort().join('').trim();
 }
-console.log(alphabetize("United States"))
+console.log(alphabetize("United States"));
+console.log(alphabetize("Rindfleisch etikettierungs überwachungs aufgaben übertragungs gesetz"));
+console.log(alphabetize("Pneumonoultramicroscopicsilicovolcanoconiosis"));
+console.log(alphabetize("الجمال"));
+console.log(alphabetize("美麗的"));
+console.log(alphabetize("美しい"));
